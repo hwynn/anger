1f6c3dbfa46d9fb867216ed4b74c520e55af521d
Harrison Wynn <harrisonwynn42@gmail.com>
2017-03-31 14:48:55 -0500

    Added matrix class again. A constructor for it has been started. It will need to be passed a 2d array when created. It's not very convenient, but I'm not feeling very clever right now.
    Signed-off-by: Harrison Wynn <harrisonwynn42@gmail.com>

---
+++ lab7/src/lab7/Matrix.java
@@ -0,0 +1,14 @@
+package lab7;
+
+
+public class Matrix {
+	private int n;
+	private double[][] mdata;
+	public Matrix(int size, double[][] inside){
+		int row;
+		mdata = new double[size][size];
+		for (row=0; row<size; row=row+1){
+			
+		}
+	}
+}
